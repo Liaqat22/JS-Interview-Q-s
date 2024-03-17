ccae39ee96edf14b2aac3847d2ca4829e9dee21b
Liaqat Ali <liaqatali097306@gmail.com>
2024-03-17 15:13:24 +0500

    update inhertance

--- inheritance.js
+++ inheritance.js
@@ -1,4 +1,7 @@
-// INHERITANCE : 
+// INHERITANCE : we have to create the parent class first i.e;(animals , vehicles) then we have to create the child classes and connect the child class to the parent class by using EXTENDS keyword for example (class cow extends animals) cow is a child class of animals class (Parent class)
+// >> we use inheritance to write less code 
+// >> we have put the common things to the parent class and
+//    rest of the things to each child class according to their nature as explained in the below examples
 
 class animals { // parent class
     alive = true
@@ -10,13 +13,13 @@ class animals { // parent class
     }
 }
 
-class cow extends animals { // child class of animal
+class cow extends animals { // child class of animals
     name = "cow"
     milk() {
         console.log('cow gives milk')
     }
 }
-class eagle extends animals { // child class of animal
+class eagle extends animals { // child class of animals
     name = "eagle"
     fly() {
         console.log('eagle can fly')
@@ -42,7 +45,7 @@ class vehicles { // parent
     type = "transport"
     runcode() {
         console.log(`
-    vahecle  : ${this.vehicletype},
+    vahecle : ${this.vehicletype},
     modal : ${this.modal},
     color : ${this.color},
     capcity : ${this.capacity}  `)
