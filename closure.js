5c5b4104e5df2133f552b69dbf6e27a2d9be610d
Liaqat Ali <liaqatali097306@gmail.com>
2024-03-22 00:09:48 +0500

    .

---
+++ closure.js
@@ -0,0 +1,20 @@
+// CLOSURE : a function with its lexical environment
+
+const main = () =>{
+    let name1 = "good morning" 
+
+const subMain = () =>{ // closure
+
+    let name = "good afternoon"
+
+    console.log(name1) // name1 is its lexical enviroment
+}
+
+ name1 = "good night" // if we change name1 value here it will return "good night" in output because it returning the variable refrence not it's hardcoded value
+
+return subMain()
+
+}
+main() // good night,  as explained above
+
+//subMain is a CLOSURE and can access name1 due to lexical enviroment even if it is initialized outside the main function (main)
---
+++ methodOverloading.js
@@ -0,0 +1,40 @@
+class parentclass{
+
+    education(){
+        console.log(`completed ICS`)
+    }
+}
+
+class childclass extends parentclass{
+    experience = '2 years'
+    
+    education(){
+        console.log("completed BSIT")
+    } // here the education is overloaded 
+}
+
+const codeOutput = new childclass()
+
+codeOutput.education() // completed BSIT ,  due to method overloading
+
+// parentclass.prototype.education() // completed ICS 
+
+
+//==================================================== function overloading ===========================================================
+
+// if we have two or more same function it will return the output of the last function as shown in below code , 
+// we have 3 same function and only the last function will run
+
+function sum(a,b){
+    return a+b
+} // function will skipped
+
+function sum(a,b,c){
+    return a+b
+}// function will skipped
+
+function sum(a,b){
+    return a*b
+} // 25
+
+console.log(sum(5,5) , "sum")
